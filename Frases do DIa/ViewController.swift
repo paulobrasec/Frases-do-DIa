f753e32b7d4f4eaf6d48628f83226ada405e72d3
Paulo Santos <paulobrasec@gmail.com>
2018-08-05 02:41:11 -0300

    Git Init
    Random quotes.

--- Frases do DIa/ViewController.swift
+++ Frases do DIa/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        
+        frases.append ("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos.")
+        
+        frases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo.")
+        
+        frases.append("Nada vale mais que o poder da persistência.")
+        
+        frases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança.")
+        
+        frases.append("Não é o peixe maior que come o peixe menor, é o peixe mais rápido que come o mais devagar.")
+        
+        let numeroAleatorio = arc4random_uniform(5)
+        legendaResultado.text = frases [ Int (numeroAleatorio) ]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
