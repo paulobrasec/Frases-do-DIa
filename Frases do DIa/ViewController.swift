57295a80f3e9b0a09bfbeb1fe9e9cf4cce1c1a6c
Paulo Santos <paulobrasec@gmail.com>
2018-08-05 01:17:50 -0300

    Initial Commit

---
+++ Frases do DIa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frases do DIa
+//
+//  Created by Paulo Cesar on 05/08/2018.
+//  Copyright © 2018 Paulo Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
